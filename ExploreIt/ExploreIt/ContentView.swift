23513c5d5870de93932f91a6f2a3a1ddd83e7527
Elyan Gutierrez <elyann60@gmail.com>
2024-08-17 17:52:26 -0500

    App now uses users location to set the default region

--- ExploreIt/ExploreIt/ContentView.swift
+++ ExploreIt/ExploreIt/ContentView.swift
@@ -10,33 +10,44 @@ import SwiftUI
 
 
 struct ContentView: View {
+    @Namespace var scopeForMap
+    @FocusState var isFocused: Bool
     
+    @State private var mapCameraPosition: MapCameraPosition = .automatic
     @State private var searchText = ""
     @State private var landmarks = [Landmark]()
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 50, longitude: 50), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
     )
-    @FocusState var isFocused: Bool
+    @State private var toggle3DMode = false
+    
+    let unitedStatesRegion = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 105.7129, longitude: -95), // Example: San Francisco
+        span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+    )
     
     var body: some View {
-        Map {
+        Map(position: $mapCameraPosition, scope: scopeForMap) {
             ForEach(landmarks, id: \.id) { landmark in
                 Marker(landmark.name, coordinate: landmark.coordinate)
             }
         }
+        .mapControls {
+            MapPitchToggle()
+        }
+        .mapStyle(.standard(elevation: .realistic))
         .overlay {
             VStack {
-                
                 Spacer()
-                    .frame(height: 40)
+                    .frame(height: 45)
                 
-                TextField("", text: $searchText, prompt: Text("Enter Location"))
-                    .frame(width: 230, alignment: .center)
+                TextField("", text: $searchText, prompt: Text("Enter City, State/Country"))
+                    .frame(width: 200, alignment: .center)
                     .tint(.black)
                     .background (
                         RoundedRectangle(cornerRadius: 15.0)
                             .fill(.regularMaterial)
-                            .frame(width: 320, height: 40)
+                            .frame(width: 270, height: 40)
                     )
                     .onSubmit {
                         Task {
@@ -46,35 +57,66 @@ struct ContentView: View {
                     .overlay {
                         RoundedRectangle(cornerRadius: 15.0)
                             .fill(.clear)
-                            .stroke(.gray.opacity(0.80), lineWidth: 2)
-                            .frame(width: 315, height: 40, alignment: .center)
+                            .stroke(.black, lineWidth: 2)
+                            .frame(width: 270, height: 40, alignment: .center)
                         
                         Image(systemName: "magnifyingglass")
                             .frame(maxWidth: .infinity, alignment: .leading)
+                            .foregroundStyle(.black)
                             .padding(.horizontal, 8)
                         
-                        Image(systemName: "xmark")
-                            .resizable()
-                            .frame(width: 7, height: 7)
-                            .fontWeight(.bold)
-                            .background(
-                                Circle()
-                                    .fill(.gray.opacity(0.60))
-                                    .frame(width: 20, height: 20)
-                                
-                            )
-                            .frame(maxWidth: .infinity, alignment: .trailing)
-                            .padding(.horizontal, 15)
+                        if !searchText.isEmpty {
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 8, height: 8)
+                                .foregroundStyle(.black)
+                                .fontWeight(.bold)
+                                .background(
+                                    Circle()
+                                        .fill(.gray.opacity(0.30))
+                                        .frame(width: 20, height: 20)
+                                    
+                                )
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                                .padding(.horizontal, 15)
+                                .onTapGesture {
+                                    resetSearchText()
+                                }
+                        }
                     }
                     .focused($isFocused)
                 
                 Spacer()
             }
         }
+        .preferredColorScheme(.light)
+        .onAppear {
+            CLLocationManager().requestWhenInUseAuthorization()
+            
+            DispatchQueue.global().async { // sets the inital user region off of the main thread
+                setInitialUserRegion()
+            }
+        }
+        .onChange(of: searchText) {
+            
+            if searchText == "" {
+                mapCameraPosition = .userLocation(fallback: .region(unitedStatesRegion))
+                landmarks = [Landmark]()
+                
+            } else {
+                mapCameraPosition = .automatic
+            }
+        }
         .onTapGesture {
             isFocused = false
         }
         
+    }
+    
+    func setInitialUserRegion() {
+        if CLLocationManager.locationServicesEnabled() {
+            mapCameraPosition = .userLocation(fallback: .region(unitedStatesRegion))
+        }
     }
     
     func returnSearchResults() async {
@@ -100,9 +142,20 @@ struct ContentView: View {
                     span: MKCoordinateSpan(latitudeDelta: 0.15, longitudeDelta: 0.15)
                 )
             }
+            
+            if !landmarks.isEmpty {
+                for landmark in landmarks {
+                    print(landmark.name)
+                }
+            }
+            
         } catch {
             print(error.localizedDescription)
         }
+    }
+    
+    func resetSearchText() {
+        searchText = ""
     }
 }
 
