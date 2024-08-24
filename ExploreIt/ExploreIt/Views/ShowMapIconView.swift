b9f27f0a7487cfbd14432c973f13e6f8c539e1a8
Elyan Gutierrez <elyann60@gmail.com>
2024-08-23 19:29:54 -0500

    Updated icon list to show icons that hold the most significance.

--- ExploreIt/ExploreIt/Views/ShowMapIconView.swift
+++ ExploreIt/ExploreIt/Views/ShowMapIconView.swift
@@ -14,7 +14,7 @@ struct ShowMapIconView: View {
     var body: some View {
         switch catagory {
         case "accommodation":
-            return  Text("🏨")
+            return Text("🏨")
         case "rental":
             return Text("🚗")
         case "hotel":
@@ -25,12 +25,6 @@ struct ShowMapIconView: View {
             return Text("🍽️")
         case "activity":
             return Text("🏃")
-        case "entertainment":
-            return Text("🍿")
-        case "sport":
-            return Text("🏃‍♂️")
-        case "leisure":
-            return Text("📖")
         case "ski":
             return Text("⛷️")
         case "tourism":
@@ -41,18 +35,12 @@ struct ShowMapIconView: View {
             return Text("✏️")
         case "childcare":
             return Text("🏫")
-        case "service":
-            return Text("🚚")
         case "natural":
             return Text("🌲")
         case "national_park":
             return Text("🏞️")
-        case "heritage":
-            return Text("🏠")
         case "camping":
             return Text("🏕️")
-        case "amenity":
-            return Text("🚽")
         case "beach":
             return Text("🏖️")
         case "pet":
@@ -65,8 +53,6 @@ struct ShowMapIconView: View {
             return Text("🏭")
         case "building":
             return Text("🏬")
-        case "public_transport":
-            return Text("🚃")
         case "railway":
             return Text("🚊")
         case "airport":
@@ -75,36 +61,12 @@ struct ShowMapIconView: View {
             return Text("🛣️")
         case "parking":
             return Text("🅿️")
-        case "man_made":
-            return Text("👷‍♂️")
         case "religion":
             return Text("⛪️")
-        case "administrative":
-            return Text("👨‍💼")
-        case "political":
-            return Text("🏩")
         case "internet_access":
             return Text("🛜")
         case "wheelchair":
             return Text("♿️")
-        case "access":
-            return Text("✅")
-        case "access_limited":
-            return Text("✔️")
-        case "no_access":
-            return Text("🚫")
-        case "fee":
-            return Text("🪙")
-        case "no_fee":
-            return Text("🆓")
-        case "named":
-            return Text("✉️")
-        case "low_emission_zone":
-            return Text("🚘")
-        case "populated_place":
-            return Text("🌆")
-        case "postal_code":
-            return Text("📮")
         case "adult":
             return Text("🍺")
         default:
